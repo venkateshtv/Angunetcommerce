ac50d6d7a2f485e3b3f25595a65faa4615f1bfa5
Coder323 <venkateshtv@outlook.com>
2016-03-16 08:56:46 +0530

    Added sngular

--- Angnetcommerce/Controllers/ProductController.cs
+++ Angnetcommerce/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Angnetcommerce.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,18 @@ namespace Angnetcommerce.Controllers
 {
     public class ProductController : ApiController
     {
+        Product[] products = new Product[]
+       {
+            new Product { Name = "Honda City", Price = 250000 },
+            new Product {Name = "Renault Scala", Price = 250000 },
+            new Product {Name = "Mercedes Benz",Price = 250000}
+       };
+
+        [HttpGet]
+        public Product[] GetProducts()
+        {
+            return products;
+        }
+
     }
 }
